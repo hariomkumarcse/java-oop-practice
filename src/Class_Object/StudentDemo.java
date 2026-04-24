f7aa8e7d7607c70f5b2a2176565a0ab67cebe97d
Hariom Kumar <hariomgok@gmail.com>
2026-04-24 23:26:59 +0530

    Writing Code in Class_Object folder StudentDemo file with main method

--- src/Class_Object/StudentDemo.java
+++ src/Class_Object/StudentDemo.java
@@ -5,6 +5,16 @@ class Student
 	
 	void display()
 	{
-		System.out.println(id + " "+name);
+		System.out.println("Id: "+id + " "+ "Name: " +name);
+	}
+}
+public class StudentDemo
+{
+	public static void main(String[] args)
+	{
+		Student s1 = new Student();
+		s1.id = 101;
+		s1.name = "Hariom";
+		s1.display();
 	}
 }
