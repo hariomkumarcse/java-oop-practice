54e451bad0f36eee45d01f8fdcf34dbbd5b35a4b
Hariom Kumar <hariomgok@gmail.com>
2026-04-25 22:49:13 +0530

    Add a folder in src Abstract and also in abstract add two folder first one is abstract class and second one is interface implement

---
+++ src/abstraction/Abstract_Class/AnimalDemo.java
@@ -0,0 +1,23 @@
+abstract class Animal
+{
+	abstract void sound();
+
+	void sleep()
+	{
+		System.out.println("Animal is sleeping");
+	}
+} 
+class Dog extends Animal
+{
+	void sound()
+	{
+		System.out.println("Dogs barks");
+	}
+}
+public class AnimalDemo
+{
+	public static void main(String[] args)
+	{
+
+	}
+}
