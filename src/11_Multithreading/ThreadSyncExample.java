0936d71468ee6381174b843497450b46f0d7d264
Hariom Kumar <hariomgok@gmail.com>
2026-05-05 17:49:22 +0530

    In ThreadSyncExample.java file add java code

---
+++ src/11_Multithreading/ThreadSyncExample.java
@@ -0,0 +1,44 @@
+class Data
+{
+	int data;
+	
+	public Data(int data)
+	{
+		this.data = data;
+	}
+
+	synchronized void displayData(String threadName)
+	{
+		System.out.println(threadName + " Start");
+		System.out.println(threadName + " data = " + data);
+		System.out.println(threadName + " End");
+	
+	}
+}
+class DataAccess implements Runnable
+{
+	Data dataObj;
+
+	DataAccess(Data dataObj)
+	{
+		this.dataObj = dataObj;
+	}
+
+	public void run()
+	{
+		dataObj.displayData(Thread.currentThread().getName());
+	}
+}
+public class ThreadSyncExample
+{
+	public static void main(String[] args)
+	{
+		Data data1 = new Data(10);
+		Thread thread1 = new Thread(new DataAccess(data1), "First Thread");
+		Thread thread2 = new Thread(new DataAccess(data1), "Second Thread");
+
+		thread1.start();
+		thread2.start();
+
+	}
+}
