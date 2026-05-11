015a8a13c062631457afe0815742366cff1d0a52
Hariom Kumar <hariomgok@gmail.com>
2026-05-11 22:35:44 +0530

    In HashSetDemo.java file update some code

--- src/12_ExamQuestions/PUT_2/HashSetDemo.java
+++ src/12_ExamQuestions/PUT_2/HashSetDemo.java
@@ -1,1 +1,15 @@
 import java.util.HashSet;
+
+public class HashSetDemo
+{
+	public static void main(String[] args)
+	{
+		HashSet<String> names = new HashSet<>();
+		
+		names.add("Hariom");
+		names.add("Ram Kumar");
+		names.add("Pradha Ji");
+		names.add("Jagan");
+		System.out.println("HashSet : " + names);
+	}	
+}
