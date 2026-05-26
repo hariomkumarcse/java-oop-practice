7204220089513ab897435561f17709b9e5bc1a3e
Hariom Kumar <hariomgok@gmail.com>
2026-05-26 21:48:48 +0530

    In List Folder add IteratorDemo.java file

---
+++ src/10_Collections/02_List/IteratorDemo.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class IteratorDemo
+{
+	public static void main(String[] args)
+	{
+		Collection<String> collection = new ArrayList<>();
+		
+		collection.add("First");
+		collection.add("Second");
+		collection.add("Third");
+		collection.add("Fourth");
+	
+		Iterator<String> itr = collection.iterator();
+		String element = null;
+		while(itr.hasNext())
+		{
+			element = itr.next();
+			System.out.println(element);
+			if(element.equals("Third"))
+			{
+				itr.remove();
+				System.out.println(" (Removed) ");
+			}
+			System.out.println(" ");
+		}
+		System.out.println(collection);
+	}
+}
