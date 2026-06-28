cee67fd8c216d3ef47aac64e172614b2dca613c3
Hariom Kumar <hariomgok@gmail.com>
2026-06-28 20:37:01 +0530

    In ArrayListBasic.java file adding java source code

---
+++ src/10_Collections/02_List/ArrayList/ArrayListBasic.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class ArrayListBasic
+{
+	public static void main(String[] args)
+	{
+		ArrayList<String> students = new ArrayList<>();
+
+		students.add("Hariom");
+		students.add("Rahul");
+		students.add("Aman");
+
+		System.out.println(students);
+	}
+}
