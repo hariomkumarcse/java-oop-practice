a00fab20a8ff88049b9bff01a913939ea942db73
Hariom Kumar <hariomgok@gmail.com>
2026-07-03 22:26:12 +0530

    In VectorBasic.java file adding java source code

---
+++ src/10_Collections/02_List/Vector/VectorBasic.java
@@ -0,0 +1,15 @@
+import java.util.Vector;
+
+public class VectorBasic
+{
+	public static void main(String[] args)
+	{
+		Vector<String> vector = new Vector<>();
+
+		vector.add("Java");
+		vector.add("Spring");
+		vector.add("SQL");
+
+		System.out.println(vector);
+	}
+}
