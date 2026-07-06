ed1ab0331c57fd49a1db642b923eea433dbb4755
Hariom Kumar <hariomgok@gmail.com>
2026-07-06 22:21:56 +0530

    In StackOperations.java file adding java source code

---
+++ src/10_Collections/02_List/Stack/StackOperations.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class StackOperations
+{
+	public static void main(String[] args)
+	{
+		Stack<Integer> stack = new Stack<>();
+
+		stack.push(10);
+		stack.push(20);
+		stack.push(30);
+
+		System.out.println("Stack: " + stack);
+
+		System.out.println("Pop: " + stack.pop());
+
+		System.out.println("Peek: " + stack.peek());
+
+		System.out.println("Search 10: " + stack.search(10));
+
+		System.out.println("Is Empty: " + stack.empty());
+
+		System.out.println("Final Stack: " + stack);
+	}
+}
