35b16b99720458e5f96c643147427da741b322e9
Hariom Kumar <hariomgok@gmail.com>
2026-07-12 22:39:02 +0530

    In RemoveDuplicates.java file adding java source code

---
+++ src/10_Collections/09_Practice_Problems/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+
+public class RemoveDuplicates
+{
+	public static void main(String[] args)
+	{
+		ArrayList<Integer> numbers = new ArrayList<>();
+
+		numbers.add(10);
+		numbers.add(20);
+		numbers.add(30);
+		numbers.add(20);
+		numbers.add(40);
+		numbers.add(10);
+
+		System.out.println("Original List: " + numbers);
+
+		LinkedHashSet<Integer> uniqueNumbers = new LinkedHashSet<>(numbers);
+
+		System.out.println("After Removing Duplicates: " + uniqueNumbers);
+	}
+}
