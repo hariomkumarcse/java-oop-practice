b4d6c5bf8bf3e38d0049ab0954dcb0d7fce0a29f
Hariom Kumar <hariomgok@gmail.com>
2026-07-16 22:17:00 +0530

    In ExceptionIntroduction.java adding java source code

---
+++ src/08_Exception_Handling/01_Basics/ExceptionIntroduction.java
@@ -0,0 +1,18 @@
+public class ExceptionIntroduction 
+{
+
+	public static void main(String[] args) 
+	{
+
+		System.out.println("Program Started");
+
+        	int a = 10;
+       		int b = 2;
+
+        	int result = a / b;
+
+        	System.out.println("Result: " + result);
+
+        	System.out.println("Program Ended");
+    }
+}
