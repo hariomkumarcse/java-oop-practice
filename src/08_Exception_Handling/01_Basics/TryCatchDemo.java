ae0ac228707c54df1b92576bbc7a62d980183479
Hariom Kumar <hariomgok@gmail.com>
2026-07-19 23:02:23 +0530

    In TryCatchDemo.java file adding java source file

---
+++ src/08_Exception_Handling/01_Basics/TryCatchDemo.java
@@ -0,0 +1,18 @@
+public class TryCatchDemo
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			int result = 10/0;
+
+			System.out.println(result);
+		}
+		catch(ArithmeticException e)
+		{
+			System.out.println("Cannot divide by zero.");
+		}
+
+		System.out.println("Program Continues...");
+	}
+}
