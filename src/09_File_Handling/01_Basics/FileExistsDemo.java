4e1ad9b5d84688a7238e845cd7fb0d93cba1e54b
Hariom Kumar <hariomgok@gmail.com>
2026-08-19 21:57:12 +0530

    In FileExistsDemo.java file adding java source code

---
+++ src/09_File_Handling/01_Basics/FileExistsDemo.java
@@ -0,0 +1,21 @@
+import java.io.File;
+
+public class FileExistsDemo
+{
+	public static void main(String[] args)
+	{
+		File file = new File("student.txt");
+
+		if(file.exists())
+		{
+			System.out.println("File exists.");
+			System.out.println("File Name: " + file.getName());
+		}
+
+		else
+		{
+			System.out.println("File does not exist.");
+		}
+	
+	}
+}
