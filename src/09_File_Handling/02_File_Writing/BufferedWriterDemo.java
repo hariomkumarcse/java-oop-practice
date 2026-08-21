ab768ef8281f46160006c75614cd1c7fbada4a0b
Hariom Kumar <hariomgok@gmail.com>
2026-08-21 20:58:51 +0530

    In BufferedWriterDemo.java file adding java source code

---
+++ src/09_File_Handling/02_File_Writing/BufferedWriterDemo.java
@@ -0,0 +1,33 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedWriterDemo
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			BufferedWriter writer = new BufferedWriter(new FileWriter("student.txt"));
+
+			writer.write("101, Hariom, Java");
+			writer.newLine();
+
+			writer.write("102, Rahul, Spring Boot");
+			writer.newLine();
+
+			writer.write("103, Aman, SQL");
+			writer.newLine();
+
+			writer.close();
+
+			System.out.println("Data written successfully.");
+		}
+
+		catch(IOException e)
+		{
+			System.out.println("Error while writing data.");
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
