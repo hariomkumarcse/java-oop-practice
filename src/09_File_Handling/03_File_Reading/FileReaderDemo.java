b473c761d8a44cd02bce550a7d892b8189634aea
Hariom Kumar <hariomgok@gmail.com>
2026-08-23 18:21:51 +0530

    In FileReaderDemo.java file adding java source code

---
+++ src/09_File_Handling/03_File_Reading/FileReaderDemo.java
@@ -0,0 +1,27 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileReaderDemo
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			FileReader reader = new FileReader("student.txt");
+
+			int character;
+
+			while((character = reader.read()) != -1)
+			{
+				System.out.println((char) character);
+			}
+
+			reader.close();
+		}
+		catch(IOException e)
+		{
+			System.out.println("Error while reading file.");
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
