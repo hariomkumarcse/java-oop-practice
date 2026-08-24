1582bf80b8be815a9cf26fb2b9bb554f58b77d61
Hariom Kumar <hariomgok@gmail.com>
2026-08-24 21:39:26 +0530

    In BufferedReaderDemo.java file adding java source code

---
+++ src/09_File_Handling/03_File_Reading/BufferedReaderDemo.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class BufferedReaderDemo
+{
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			BufferedReader reader = new BufferedReader(new FileReader("students.txt"));
+
+			String line;
+
+			while((line = reader.readLine()) != null)
+			{
+				System.out.println(line);
+			}
+
+			reader.close();
+		}
+
+		catch(IOException e)
+		{
+			System.out.println("Error while reading file.");
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
