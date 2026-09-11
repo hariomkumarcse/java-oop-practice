73d5bb3e243f0b9114e67d0b708e71b3876ebc80
Hariom Kumar <hariomgok@gmail.com>
2026-09-11 18:34:09 +0530

    In SerializationDemo.java file adding java source code

---
+++ src/09_File_Handling/09_Object_Serialization/SerializationDemo.java
@@ -0,0 +1,51 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class SerializationDemo
+{
+	static class Student implements Serializable 
+	{
+		private static final long serialVersionUID = 1L;
+
+		int id;
+		String name;
+		double marks;
+
+		Student(int id, String name, double marks)
+		{
+			this.id = id;
+			this.name = name;
+			this.marks = marks;
+		}
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			Student student = new Student(
+						101,
+						"Hariom",
+						85.5
+						);
+
+			FileOutputStream fileOutput = new FileOutputStream("student.ser");
+
+			ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput);
+
+			objectOutput.writeObject(student);
+
+			objectOutput.close();
+			fileOutput.close();
+
+			System.out.println("Object serialized successfully.");
+		}
+		catch(IOException e)
+		{
+			System.out.println("Error during serialization.");
+			System.out.println("Error: " + e.getMessage());
+		}
+	}
+}
